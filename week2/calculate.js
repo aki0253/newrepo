ae1b8a8d7577e95763196c76e4e7f2507fc14254
Aki9474 <akriti.poudyal@westcliff.edu>
2026-05-12 07:54:23 +0545

    week2 first commit

---
+++ week2/calculate.js
@@ -0,0 +1,13 @@
+// Take two integer inputs from the user
+let num1 = parseInt(prompt("Enter the first number:"));
+let num2 = parseInt(prompt("Enter the second number:"));
+
+// Add the numbers
+let addition = num1 + num2;
+
+// Subtract the numbers
+let subtraction = num1 - num2;
+
+// Display the results
+console.log("Addition:", addition);
+console.log("Subtraction:", subtraction);
